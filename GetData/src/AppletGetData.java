20875ab3ebca1b5a80f470e02853d696e9592a51
Damian Cat <dragon1600@gmail.com>
2015-11-04 19:25:46 +0100

    Eye-tracking applet

--- GetData/src/AppletGetData.java
+++ GetData/src/AppletGetData.java
@@ -1,52 +1,115 @@
-import java.awt.Graphics;
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-import java.io.Writer;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import javax.swing.JApplet;
+import java.util.Date;
 
-public class AppletGetData extends JApplet
+
+public class AppletGetData extends JApplet implements Runnable
 {
+	private int screenX = 1920;
+	private int screenY = 1080;
+	private int n = 1;
+	Thread writeToFile;
+	Date date;
+	boolean running = true;
+	File file = new File("D:\\Desktop\\Medialogy\\P3\\DataFiles\\Test"+n+".txt");
+	
+	public void AppletGetData ()
+	{
+		
+	}
 	public void init ()
 	{
+		//Initiate the GetData class to gather data and the writeToFile thread to put the data in a textfile.
 		GetData getData = new GetData();
-		try {
-			toFile();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	public void paint(Graphics g)
-	{
-		String gXString = String.valueOf(GetData.gX);
-		String gYString = String.valueOf(GetData.gY);
-		super.paint(g);
-		g.drawString("x is: "+ gXString +"   and y is:  "+ gYString, 20, 20);
-		repaint();
-	}
-	public void toFile() throws IOException
-	{
-			
-		File file = new File("D:\Program Files (x86)\Eclipse\Workspace\GetData\test1.txt");
-			if (!file.exists())
-			{
-				file.createNewFile();
-			}
-			if (file.exists())
-			{
-				System.out.println("Hello?");
-				FileWriter printOut = new FileWriter(file);
-				printOut.write("x = " + GetData.gX+ " y = "+ GetData.gY);
-				printOut.flush();
-				printOut.close();
-				
-			}
-			
+		writeToFile = new Thread(this);
+		writeToFile.start();
+
 		
+		
+	}
+	public void destroy ()
+	{
+		running = false;
+		writeToFile = null;
+	}
+	
+	public void run()
+	{
+		BufferedWriter printOut = null;
+		
+		// If the filename already exists, it calls the method to do some recursion.
+		if (file.exists())
+		{
+			checkFile(file);
+		}
+		
+		if (!file.exists())
+		{
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		try {
+			printOut = new BufferedWriter(new FileWriter(file));
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		while(running)
+		{
+			//Get the time.
+			date = new Date();
+			String time = date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+			
+				try {
+					// if looking at screen.
+					if (GetData.gX > 0 && GetData.gY > 0 && GetData.gX < screenX && GetData.gY < screenY)
+					{
+						//printout x and y locations to file.
+						printOut.write("x = " + GetData.gX+ " y = "+ GetData.gY +" at time: " + time);
+						printOut.newLine();
+						printOut.flush();
+					}
+					
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				try
+	               {
+	                     // Wait 250 milliseconds before continuing
+	                    writeToFile.sleep(250);
+	               }
+	               catch (InterruptedException e)
+	               {
+	                    System.out.println(e);
+	                }
+			
+			
+		}
+		
+	}
+	
+	//a recursive method that keeps adding 1 to n until the filename is free.
+	public void checkFile(File file)
+	{
+		if (file.exists())
+		{
+			n++;
+			file = new File("D:\\Desktop\\Medialogy\\P3\\DataFiles\\Test"+n+".txt");
+			this.file = file;
+			checkFile(file);
+		}
 	}
 }
--- GetData/src/GetData.java
+++ GetData/src/GetData.java
@@ -1,7 +1,3 @@
-import java.awt.event.KeyEvent;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-
 import com.theeyetribe.client.GazeManager;
 import com.theeyetribe.client.IGazeListener;
 import com.theeyetribe.client.data.GazeData;
@@ -10,14 +6,7 @@ public class GetData  implements IGazeListener
 {
 	public static double gX;
 	public static double gY;
-	private int x = 1920;
-	private int y = 1080;
-	private int framesLookedAtRight;
-	private int framesLookedAtLeft;
-	private int rightSeconds = framesLookedAtRight/30;
-	private int leftSeconds = framesLookedAtLeft/30;
 	
-      
       
       GetData()
       {
@@ -28,27 +17,7 @@ public class GetData  implements IGazeListener
       
       public void onGazeUpdate(GazeData gazeData)
       {
-         //double gX = gazeData.rawCoordinates.x;
-         //double gY = gazeData.rawCoordinates.y;
          gX = gazeData.smoothedCoordinates.x;
          gY = gazeData.smoothedCoordinates.y;
-         
-         
-         /*try 
-         {
-			PrintWriter printOut = new PrintWriter(fileName);
-			printOut.println("x = " + gX+ " y = "+ gY);
-			if (e.getKeyCode() == KeyEvent.VK_ALT)
-			 {
-				 printOut.println("Times looked at right: "+ rightSeconds);
-				 printOut.println("Times looked at left: "+ leftSeconds);
-				 printOut.close();
-				 System.exit(0);
-			 }
-		} catch (FileNotFoundException e) 
-         {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
       }
 }
