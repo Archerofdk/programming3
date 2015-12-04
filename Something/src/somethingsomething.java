dca964d24087499a231191514c3984634adcfbe8
unknown <Dragon1600@gmail.com>
2015-12-04 10:53:13 +0100

    Heatmap is in 'something'

--- Something/src/somethingsomething.java
+++ Something/src/somethingsomething.java
@@ -1,16 +1,59 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.opencv.core.Core;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-
-
-public class somethingsomething 
+import org.opencv.core.Scalar;
+import org.opencv.core.CvType;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.highgui.Highgui;
+	
+public class somethingsomething
 {
-	
-	public static void main( String[] args )
-	{
-		System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
-	    Mat mat = Mat.eye( 3, 3, CvType.CV_8UC1 );
-	    System.out.println( "mat = " + mat.dump() );
-	}
-	
+
+		private static int n = 1;
+		static String last;
+		static { System.loadLibrary( Core.NATIVE_LIBRARY_NAME ); }
+		
+		/*public static void main(String[] args) throws IOException 
+		{
+			
+		}*/
+		public void run ()
+		{
+			Mat image = Highgui.imread(getClass().getResource("/lena.png").getPath());
+		}
+		
+		
+		public static void ReadTest () throws IOException
+		{
+			
+			//for (int n = 1; n<123; n++)
+			//{
+				BufferedReader input = new BufferedReader(new FileReader("D:\\Desktop\\Medialogy\\P3\\DataFiles\\Test"+n+".txt"));
+				String line;
+				
+				while ((line = input.readLine()) != null) 
+			    {
+			        last = line;
+			        String[] PageData = last.split(";");
+			      
+			        //fra 0 til 6
+			        double FirstCheck = Double.parseDouble (PageData[4]);
+			        if (FirstCheck != 0)
+			        {
+			        	String PageLocation = PageData[2];
+			        	if (PageLocation.equals("http://testserver3.weebly.com/"))
+			        	{
+			        		
+			        		  
+			        	
+			        		
+			        	}
+			        }
+			    }
+			//}
+		}
 }
